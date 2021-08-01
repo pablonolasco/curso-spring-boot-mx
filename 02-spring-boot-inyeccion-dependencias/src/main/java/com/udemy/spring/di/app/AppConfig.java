afee2f0fcc91c98c016e877dde0d12632c15d69b
pablonolasco <pablo.nolasco@live.com.mx>
2021-07-31 19:13:46 -0500

    Registrando componentes usando clase de configuración y la anotación @Bean

--- 02-spring-boot-inyeccion-dependencias/src/main/java/com/udemy/spring/di/app/AppConfig.java
+++ 02-spring-boot-inyeccion-dependencias/src/main/java/com/udemy/spring/di/app/AppConfig.java
@@ -1,14 +1,25 @@
 package com.udemy.spring.di.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.udemy.spring.di.app.service.IService;
 import com.udemy.spring.di.app.service.MiServicio;
+import com.udemy.spring.di.app.service.MiServicioComplejo;
 
 @Configuration
 public class AppConfig {
 
+	//== Se registran los beans
+	@Bean("miServicioSimple")
+	@Primary
 	public IService registrarServicio() {
 		return new MiServicio();
+	}
+	
+	@Bean("miServicioComplejo")
+	public IService registrarServicioComplejo() {
+		return new MiServicioComplejo();
 	}
 }
--- 02-spring-boot-inyeccion-dependencias/src/main/java/com/udemy/spring/di/app/service/MiServicio.java
+++ 02-spring-boot-inyeccion-dependencias/src/main/java/com/udemy/spring/di/app/service/MiServicio.java
@@ -3,9 +3,9 @@ package com.udemy.spring.di.app.service;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 //== Es un calificador por si hay mas de una implementacion
-@Component("miServicio")
+//@Component("miServicio")
 //== Se utiliza para colocar por prdeterminada esta implementacion si existe mas de una clase que implmente la misma interfaz
-@Primary
+//@Primary
 public class MiServicio implements IService{
 
 	@Override
