2d66cb838c737fd1d2a84b159204207fe6c6a19d
pablonolasco <pablo.nolasco@live.com.mx>
2021-08-01 16:56:05 -0500

    Ejemplo Factura y relaciones entre sus componentes

--- 02-spring-boot-inyeccion-dependencias/src/main/java/com/udemy/spring/di/app/models/domain/Factura.java
+++ 02-spring-boot-inyeccion-dependencias/src/main/java/com/udemy/spring/di/app/models/domain/Factura.java
@@ -3,10 +3,13 @@ package com.udemy.spring.di.app.models.domain;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Factura {
+	
+	@Value("${factura.descripcion}")
 	private String descripcion;
 	@Autowired
 	private Cliente cliente;
