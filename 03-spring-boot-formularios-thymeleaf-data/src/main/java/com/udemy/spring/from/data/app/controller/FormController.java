c3f5071404a35d686124a079cba60a70f50c1435
Pablo <pablo.nolasco@live.com.mx>
2021-08-12 20:04:22 -0500

    Método handler para recibir y procesar los datos enviados

--- 03-spring-boot-formularios-thymeleaf-data/src/main/java/com/udemy/spring/from/data/app/controller/FormController.java
+++ 03-spring-boot-formularios-thymeleaf-data/src/main/java/com/udemy/spring/from/data/app/controller/FormController.java
@@ -5,19 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/forms")
 public class FormController {
 
     @GetMapping("/data")
-    public String index(Model model){
-        model.addAttribute("titulo","Formulario");
-        return "index";
+    public String index(Model model) {
+        model.addAttribute("titulo", "Formulario");
+        return "form";
     }
 
     @PostMapping("/frmProcesar")
-    public String form(){
-        return "";
+    public String form(Model model, @RequestParam("user") String nombre, @RequestParam String password,
+                       @RequestParam String email) {
+        model.addAttribute("titulo","Datos enviados");
+        model.addAttribute("user",nombre);
+        model.addAttribute("password",password);
+        model.addAttribute("email",email);
+        return "index";
     }
 }
